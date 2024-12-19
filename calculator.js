6a12165e41ac08a26731b5efb4936fcb49490de4
TitusBokosi <titusbokosi@gmail.com>
2024-12-19 12:00:30 +0200

    added the holding variables

--- calculator.js
+++ calculator.js
@@ -6,16 +6,34 @@ const inputDiv = document.createElement('div');
 mainDiv.appendChild(inputDiv);
 inputDiv.appendChild(input)
 mainDiv.appendChild(btnsDiv);
-const digitHolder =new Array(10);
-for(i = 0; i <= 9; i++){
-    const btn = document.createElement('button');
-    btn.textContent = i.toString();
-    digitHolder[i] = btn;
-    btnsDiv.appendChild(btn);
-    // btn.addEventListener('click', () => {
-        // input.value = btn.textContent;
-    // })
+
+const one = document.createElement('button');
+const two = document.createElement('button');
+const three = document.createElement('button');
+const four = document.createElement('button');
+const five = document.createElement('button');
+const six = document.createElement('button');
+const seven = document.createElement('button');
+const eight = document.createElement('button');
+const nine = document.createElement('button');
+const zero = document.createElement('button');
+const digitHolder = [one, two, three, four, five, six, seven, eight, nine, zero];
+const digitText = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+for(i = 0; i < digitHolder.length; i++){
+    btnsDiv.appendChild(digitHolder[i]);
+    digitHolder[i].textContent = digitText[i];
 }
+
+// for(i = 0; i <= 9; i++){
+//     const btn = document.createElement('button');
+//     btn.textContent = i.toString();
+//     digitHolder[i] = btn;
+//     btnsDiv.appendChild(btn);
+//     // btn.addEventListener('click', () => {
+//         // input.value = btn.textContent;
+//     // })
+// }
 
 
 const mutlbtn = document.createElement('button');
@@ -35,16 +53,28 @@ for(i = 0; i < textContents.length; i++){
     actionbtns[i].textContent = textContents[i];
 }
 
-digitHolder.forEach(button => {
-    button.addEventListener('click', () => {
-        input.value = button.textContent;
+
+let firstNUmber = '';
+let operator = '';
+let secondNumber = '';
+let display = '';
+
+
+digitHolder.forEach(buton => {
+    buton.addEventListener('click', () => {
+       display = display.concat(buton.textContent);
+       input.value = display;
     })
 })
 
 actionbtns.forEach(button => {
     button.addEventListener('click', () => {
-        input.value = button.textContent;
+        display = display.concat(button.textContent);
+        operator = button.textContent;
+        input.value = display;
     })
-})
+});
+
+
 
 
